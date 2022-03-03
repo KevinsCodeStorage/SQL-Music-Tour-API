a8dc3d04dddd9becf0ab55a3c6b6e037381279c5
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2022-03-02 20:52:49 -0500

    new seed

--- seeders/20210805164045-bands.js
+++ seeders/20210805164045-bands.js
@@ -1,17 +1,32 @@
-'use strict'
+"use strict";
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('bands', [{
-      name: 'The Useful Books',
-      genre: 'Rock',
-      available_start_time: '11:00:00',
-      end_time: '23:00:00'
-    }])
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("events", [
+      {
+        name: "OceanSide",
+        available_start_time: "2022-01-01T12:00:00",
+        end_time: "2022-01-01T19:30:00",
+      },
+      {
+        name: "CLiffSide",
+        available_start_time: "2022-02-01T12:00:00",
+        end_time: "2022-01-01T19:30:00",
+      },
+      {
+        name: "SkySide",
+        available_start_time: "2022-03-01T12:00:00",
+        end_time: "2022-01-01T19:30:00",
+      },
+    ]);
   },
 
-  down: async (queryInterface, Sequelize) => {
-    // note that this deletes ALL data from the bands table
-    await queryInterface.bulkDelete('bands', null, {})
-  }
-}
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
